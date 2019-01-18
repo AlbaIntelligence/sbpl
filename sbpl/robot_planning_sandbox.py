99f7c5798d31ce193223d4e72e04f6a46b543305
olegsinyavskiy <olegsinyavskiy@gmail.com>
2019-01-18 11:01:57 -0800

    no dependecy

--- sbpl/robot_planning_sandbox.py
+++ sbpl/robot_planning_sandbox.py
@@ -5,10 +5,9 @@ from __future__ import division
 import numpy as np
 
 
-from bc_gym_planning_env.envs.rw_corridors.tdwa_test_environments import \
-    get_random_maps_squeeze_between_obstacle_in_corridor_on_path
 from sbpl.motion_primitives import forward_model_diffdrive_motion_primitives
 from sbpl.planners import perform_single_planning
+from sbpl.utilities.costmap_2d_python import CostMap2D
 from sbpl.utilities.differential_drive import industrial_diffdrive_footprint
 from sbpl.utilities.map_drawing_utils import add_wall_to_static_map
 
@@ -19,9 +18,14 @@ def run_sbpl_motiont_primitive_planning_benchmark(
         w_samples_in_each_direction,
         primitives_duration,
         footprint_scale):
-    original_costmap, static_path, test_maps = get_random_maps_squeeze_between_obstacle_in_corridor_on_path()
 
-    test_map = test_maps[0]
+    test_map = CostMap2D(
+        data=np.zeros((140, 70), dtype=np.uint8),
+        origin=np.array([0., 0.]),
+        resolution=0.03
+    )
+    test_map.get_data()[:] = 0
+    print(test_map.get_origin(), test_map.get_data().shape)
     resolution = test_map.get_resolution()
 
     motion_primitives = forward_model_diffdrive_motion_primitives(
@@ -33,7 +37,13 @@ def run_sbpl_motiont_primitive_planning_benchmark(
         primitives_duration=primitives_duration
     )
 
-    add_wall_to_static_map(test_map, (1, -4.6), (1.5, -4.6))
+    add_wall_to_static_map(test_map, (0.07828677, 2.15250846), (0.07828677, 3.95250846))
+    add_wall_to_static_map(test_map, (0.97828677, 2.15250846), (1.47828677, 2.15250846))
+
+    start_pose = np.array([0.9818883, 3.52881533, -np.pi/2])
+    goal_pose = np.array([0.97828677, 1.56319919, -np.pi/2])
+    print(start_pose)
+    print(goal_pose)
 
     plan_xytheta, plan_xytheta_cell, actions, plan_time, solution_eps, environment = perform_single_planning(
         planner_name='arastar',
@@ -41,12 +51,12 @@ def run_sbpl_motiont_primitive_planning_benchmark(
         motion_primitives=motion_primitives,
         forward_search=True,
         costmap=test_map,
-        start_pose=static_path[0],
-        goal_pose=static_path[-10],
+        start_pose=start_pose,
+        goal_pose=goal_pose,
         target_v=target_v,
         target_w=target_w,
         allocated_time=np.inf,
-        cost_scaling_factor=4.,
+        cost_scaling_factor=1.,
         debug=True)
 
 
@@ -55,7 +65,7 @@ if __name__ == '__main__':
         target_v=0.65,
         target_w=1.0,
         number_of_angles=32,
-        w_samples_in_each_direction=4,
+        w_samples_in_each_direction=2,
         primitives_duration=5,
         footprint_scale=1.8
     )
