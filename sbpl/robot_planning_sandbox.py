ebd12989558eee19f393a3a791a245aed24bdbad
olegsinyavskiy <olegsinyavskiy@gmail.com>
2019-01-18 09:49:09 -0800

    reproduce collisions in sbpl only

--- sbpl/robot_planning_sandbox.py
+++ sbpl/robot_planning_sandbox.py
@@ -1,59 +1,61 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import numpy as np
 
 
-def run_sbpl_motiont_primitive_planning_benchmark(config):
+from bc_gym_planning_env.envs.rw_corridors.tdwa_test_environments import \
+    get_random_maps_squeeze_between_obstacle_in_corridor_on_path
+from sbpl.motion_primitives import forward_model_diffdrive_motion_primitives
+from sbpl.planners import perform_single_planning
+from sbpl.utilities.differential_drive import industrial_diffdrive_footprint
+from sbpl.utilities.map_drawing_utils import add_wall_to_static_map
+
+
+def run_sbpl_motiont_primitive_planning_benchmark(
+        number_of_angles,
+        target_v, target_w,
+        w_samples_in_each_direction,
+        primitives_duration,
+        footprint_scale):
     original_costmap, static_path, test_maps = get_random_maps_squeeze_between_obstacle_in_corridor_on_path()
-    robot = create_robot(config['robot_name'], footprint_scale=config['footprint_scale'])
 
-    resolution = test_maps[0].get_resolution()
+    test_map = test_maps[0]
+    resolution = test_map.get_resolution()
 
     motion_primitives = forward_model_diffdrive_motion_primitives(
         resolution=resolution,
-        number_of_angles=config['number_of_angles'],
-        target_v=config['target_v'],
-        target_w=config['target_w'],
-        w_samples_in_each_direction=config['w_samples_in_each_direction'],
-        primitives_duration=config['primitives_duration']
+        number_of_angles=number_of_angles,
+        target_v=target_v,
+        target_w=target_w,
+        w_samples_in_each_direction=w_samples_in_each_direction,
+        primitives_duration=primitives_duration
     )
 
-    def _run_planning(test_map):
-        add_wall_to_static_map(test_map, (1, -4.6), (1.5, -4.6))
+    add_wall_to_static_map(test_map, (1, -4.6), (1.5, -4.6))
 
-        plan_xytheta, plan_xytheta_cell, actions, plan_time, solution_eps, environment = perform_single_planning(
-            planner_name='arastar',
-            footprint=robot.get_footprint(),
-            motion_primitives=motion_primitives,
-            forward_search=True,
-            costmap=test_map,
-            start_pose=static_path[0],
-            goal_pose=static_path[-10],
-            target_v=config['target_v'],
-            target_w=config['target_w'],
-            allocated_time=np.inf,
-            cost_scaling_factor=4.,
-            debug=True)
+    plan_xytheta, plan_xytheta_cell, actions, plan_time, solution_eps, environment = perform_single_planning(
+        planner_name='arastar',
+        footprint=industrial_diffdrive_footprint(footprint_scaler=footprint_scale),
+        motion_primitives=motion_primitives,
+        forward_search=True,
+        costmap=test_map,
+        start_pose=static_path[0],
+        goal_pose=static_path[-10],
+        target_v=target_v,
+        target_w=target_w,
+        allocated_time=np.inf,
+        cost_scaling_factor=4.,
+        debug=True)
 
-        return len(plan_xytheta) > 0, plan_time
 
-    def _progress_callback(done, total):
-        print("Done %d out of %d" % (done, total))
-
-    start_time = time.time()
-    results = multiprocessing_map(_run_planning, test_maps, progress_callback=_progress_callback, n_workers=1)
-    successes = [r[0] for r in results]
-    planning_times = [r[1] for r in results]
-
-    print(time.time() - start_time, np.sum(successes), np.mean(planning_times))
-    '''
+if __name__ == '__main__':
     run_sbpl_motiont_primitive_planning_benchmark(
-        dict(label='sbpl_diffdrive_forward_model',
-             planner_name='sbpl_path_forward',
-             robot_name=RobotNames.INDUSTRIAL_DIFFDRIVE_V1,
-             footprint_scale=1.8,
-             target_v=0.65,
-             target_w=1.0,
-             number_of_angles=32)
+        target_v=0.65,
+        target_w=1.0,
+        number_of_angles=32,
+        w_samples_in_each_direction=4,
+        primitives_duration=5,
+        footprint_scale=1.8
     )
-    '''
-    # 124.759207964 377 0.7519494995
-    # 540.658827782 480 1.82676428372 (w_samples_in_each_direction=16,
-    # 176.419268847 415 0.61856025974 (w_samples_in_each_direction=4
--- sbpl/utilities/differential_drive.py
+++ sbpl/utilities/differential_drive.py
@@ -64,3 +64,40 @@ def diff_drive_control_to_tricycle(linear_vel, angular_vel, front_wheel_angle,
         front_wheel_linear_velocity = 0.
 
     return front_wheel_linear_velocity, desired_angle
+
+
+def industrial_diffdrive_footprint(footprint_scaler):
+    # Note: This is NOT the real footprint, just a mock for the simulator in order to develop a strategy
+    footprint = np.array([
+        [644.5, 0],
+        [634.86, 61],
+        [571.935, 130.54],
+        [553.38, 161],
+        [360.36, 186],
+        # Right attachement
+        [250, 186],
+        [250, 186],
+        [100, 186],
+        [100, 186],
+        # End of right attachement
+        [0, 196],
+        [-119.21, 190.5],
+        [-173.4, 146],
+        [-193, 0],
+        [-173.4, -143],
+        [-111.65, -246],
+        [-71.57, -246],
+        # Left attachement
+        [100, -246],
+        [100, -246],
+        [250, -246],
+        [250, -246],
+        # End of left attachement
+        [413.085, -223],
+        [491.5, -204.5],
+        [553, -161],
+        [634.86, -62]
+    ]) / 1000.
+
+    assert (footprint[0, 1] == 0)  # bumper front-center has to be the first one (just so that everything is correct)
+    return footprint*footprint_scaler
