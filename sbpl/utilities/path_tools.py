321ceb2cb0b0057c76b99e4ffd09d474168c7c78
olegsinyavskiy <olegsinyavskiy@gmail.com>
2019-03-17 18:39:25 -0700

    some simplifications

--- sbpl/utilities/path_tools.py
+++ sbpl/utilities/path_tools.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from __future__ import division
 
 import numpy as np
-from bc_gym_planning_env.utilities.coordinate_transformations import normalize_angle, world_to_pixel
+from bc_gym_planning_env.utilities.coordinate_transformations import normalize_angle, world_to_pixel, pixel_to_world
 
 
 def world_to_pixel_sbpl(world_coords, origin, resolution):
@@ -52,16 +52,7 @@ def pixel_to_world_centered(pixel_coords, origin, resolution):
     return pixel_coords.astype(np.float64) * resolution + np.array(origin, dtype=np.float64) + resolution*0.5
 
 
-def normalize_angle_0_2pi(angle):
-    # get to the range from -2PI, 2PI
-    if np.abs(angle) > 2 * np.pi:
-        angle = angle - ((int)(angle / (2 * np.pi))) * 2 * np.pi
-
-    # get to the range 0, 2PI
-    if angle < 0:
-        angle += 2 * np.pi
-
-    return angle
+pixel_to_world_centered = pixel_to_world
 
 
 def angle_cont_to_discrete(angle, num_angles):
@@ -72,7 +63,7 @@ def angle_cont_to_discrete(angle, num_angles):
     :return: discrete angle
     '''
     theta_bin_size = 2.0 * np.pi / num_angles
-    return (int)(normalize_angle_0_2pi(angle + theta_bin_size / 2.0) / (2.0 * np.pi) * num_angles)
+    return (int)((normalize_angle(angle + theta_bin_size / 2.0) + 2*np.pi) / (2.0 * np.pi) * num_angles)
 
 
 def angle_discrete_to_cont(angle_cell, num_angles):
